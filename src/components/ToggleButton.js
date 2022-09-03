4f40ef4343ee137dbcd0d9d7243e760e9873bd19
Sarina Li <sarinajin.li@gmail.com>
2022-09-02 21:40:04 -0400

    tried some button styling (half works)

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,7 +1,16 @@
+import styled from 'styled-components';
 import './css/ToggleButton.css';
+import './Themes.js';
+
+const Button = styled.button`
+    background: ${({ theme }) => theme.button_colour};
+    border-color: ${({ theme }) => theme.border_colour};
+    border: none;
+    box-shadow: ${({ theme }) => theme.button_boxshadow};
+`;
 
 function ToggleButton() {
-    return <a className = "displayMode" onClick={toggleDisplay}></a>
+    return <Button className = "displayMode" onClick={toggleDisplay}></Button>
 }
 
 export default ToggleButton;
